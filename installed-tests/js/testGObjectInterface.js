1ce465f3159090488096e9db0a7546e4aaf28f60
Philip Chimento <philip@endlessm.com>
2015-06-15 11:57:34 -0700

    gobject: GObject.Interface
    This adds the ability to define new interfaces in GJS that are known to
    GObject and that have signals and properties. Objects implementing a
    GObject.Interface get the interface's signals automatically. They must,
    however, reimplement the interface's properties or an error will occur.
    This is just as in C, except there we conveniently have
    g_param_spec_override(). In a later commit we will add a
    GObject.ParamSpec.override() to make this less painful in GJS.
    
    This also refactors some code that is now common to both classes and
    interfaces into reusable functions, both on the C and Javascript side
    of things. Where possible without too much churn, it uses newer
    SpiderMonkey APIs in code that it touches.
    
    (Philip & Roberto)
    
    [endlessm/eos-sdk#3258]

--- installed-tests/js/testGObjectInterface.js
+++ installed-tests/js/testGObjectInterface.js
@@ -1,9 +1,11 @@
 // -*- mode: js; indent-tabs-mode: nil -*-
 
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 const JSUnit = imports.jsUnit;
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 
 const MyInterface = new Lang.Interface({
     Name: 'MyInterface',
@@ -16,6 +18,106 @@ const MyInterfaceGObject = new Lang.Class({
 
     _init: function (props={}) {
         this.parent(props);
+    }
+});
+
+const MyGObjectInterface = new Lang.Interface({
+    Name: 'MyGObjectInterface',
+    GTypeName: 'ArbitraryGTypeName',
+    Requires: [ GObject.Object ],
+    Properties: {
+        'interface-prop': GObject.ParamSpec.string('interface-prop',
+            'Interface property', 'Must be overridden in implementation',
+            GObject.ParamFlags.READABLE,
+            'foobar')
+    },
+    Signals: {
+        'interface-signal': {}
+    },
+
+    requiredG: Lang.Interface.UNIMPLEMENTED,
+    optionalG: function () {
+        return 'MyGObjectInterface.optionalG()';
+    }
+});
+
+const MyOtherGObjectInterface = new Lang.Interface({
+    Name: 'MyOtherGObjectInterface',
+    Requires: [ MyGObjectInterface ],
+
+    optionalG: function () {
+        return 'MyOtherGObjectInterface.optionalG()\n' +
+            MyGObjectInterface.optionalG(this);
+    }
+});
+
+const MyInitableInterface = new Lang.Interface({
+    Name: 'MyInitableInterface',
+    Requires: [ GObject.Object, Gio.Initable ]
+});
+
+const MyGObject = new Lang.Class({
+    Name: 'MyGObject',
+    Extends: GObject.Object,
+    Implements: [ MyGObjectInterface ],
+    Properties: {
+        'interface-prop': GObject.ParamSpec.string('interface-prop', 'override',
+            'override', GObject.ParamFlags.READABLE, 'foobar'),
+        'class-prop': GObject.ParamSpec.string('class-prop', 'Class property',
+            'A property that is not on the interface',
+            GObject.ParamFlags.READABLE, 'meh')
+    },
+    Signals: {
+        'class-signal': {},
+    },
+
+    get interface_prop() {
+        return 'foobar';
+    },
+
+    get class_prop() {
+        return 'meh';
+    },
+
+    _init: function (props={}) {
+        this.parent(props);
+    },
+    requiredG: function () {},
+    optionalG: function () {
+        return MyGObjectInterface.optionalG(this);
+    }
+});
+
+const MyMinimalGObject = new Lang.Class({
+    Name: 'MyMinimalGObject',
+    Extends: GObject.Object,
+    Implements: [ MyGObjectInterface ],
+    Properties: {
+        'interface-prop': GObject.ParamSpec.string('interface-prop', 'override',
+            'override', GObject.ParamFlags.READABLE, 'foobar')
+    },
+
+    _init: function (props={}) {
+        this.parent(props);
+    },
+    requiredG: function () {}
+});
+
+const MyOtherGObject = new Lang.Class({
+    Name: 'MyOtherGObject',
+    Extends: GObject.Object,
+    Implements: [ MyGObjectInterface, MyOtherGObjectInterface ],
+    Properties: {
+        'interface-prop': GObject.ParamSpec.string('interface-prop', 'override',
+            'override', GObject.ParamFlags.READABLE, 'foobar')
+    },
+
+    _init: function (props={}) {
+        this.parent(props);
+    },
+    requiredG: function () {},
+    optionalG: function () {
+        return MyOtherGObjectInterface.optionalG(this);
     }
 });
 
@@ -34,6 +136,197 @@ function testGObjectCanImplementInterfacesFromJSAndC() {
         }
     });
     new MyHybridObject();
+}
+
+function testGObjectInterfaceIsInstanceOfInterfaces() {
+    JSUnit.assertTrue(MyGObjectInterface instanceof Lang.Interface);
+    JSUnit.assertTrue(MyGObjectInterface instanceof GObject.Interface);
+}
+
+function testGObjectInterfaceCannotBeInstantiated() {
+    JSUnit.assertRaises(() => new MyGObjectInterface());
+}
+
+function testGObjectInterfaceTypeName() {
+    JSUnit.assertEquals('ArbitraryGTypeName', MyGObjectInterface.$gtype.name);
+}
+
+function testGObjectCanImplementInterface() {
+    new MyGObject();
+}
+
+function testGObjectImplementingInterfaceHasCorrectClassObject() {
+    JSUnit.assertEquals('[object GObjectClass for MyGObject]', MyGObject.toString());
+    let obj = new MyGObject();
+    JSUnit.assertEquals(MyGObject, obj.constructor);
+    JSUnit.assertEquals('[object GObjectClass for MyGObject]',
+        obj.constructor.toString());
+}
+
+function testGObjectCanImplementBothGObjectAndNonGObjectInterfaces() {
+    const MyHybridGObject = new Lang.Class({
+        Name: 'MyHybridGObject',
+        Extends: GObject.Object,
+        Implements: [ MyInterface, MyGObjectInterface ],
+        Properties: {
+            'interface-prop': GObject.ParamSpec.string('interface-prop',
+                'override', 'override', GObject.ParamFlags.READABLE, 'foobar')
+        },
+
+        _init: function (props={}) {
+            this.parent(props);
+        },
+        required: function () {},
+        requiredG: function () {}
+    });
+    new MyHybridGObject();
+}
+
+function testGObjectCanImplementRequiredFunction() {
+    let obj = new MyGObject();
+    obj.requiredG();
+}
+
+function testGObjectMustImplementRequiredFunction () {
+    JSUnit.assertRaises(() => new Lang.Class({
+        Name: 'MyBadObject',
+        Extends: GObject.Object,
+        Implements: [ MyGObjectInterface ],
+        Properties: {
+            'interface-prop': GObject.ParamSpec.string('interface-prop',
+                'override', 'override', GObject.ParamFlags.READABLE, 'foobar')
+        }
+    }));
+}
+
+function testGObjectDoesntHaveToImplementOptionalFunction() {
+    new MyMinimalGObject();
+}
+
+function testGObjectCanDeferToInterfaceOptionalFunction() {
+    let obj = new MyMinimalGObject();
+    JSUnit.assertEquals('MyGObjectInterface.optionalG()', obj.optionalG());
+}
+
+function testGObjectCanChainUpToInterface() {
+    let obj = new MyGObject();
+    JSUnit.assertEquals('MyGObjectInterface.optionalG()', obj.optionalG());
+}
+
+function testGObjectInterfaceCanRequireOtherInterface() {
+    new MyOtherGObject();
+}
+
+function testGObjectInterfaceCanChainUpToOtherInterface() {
+    let obj = new MyOtherGObject();
+    JSUnit.assertEquals('MyOtherGObjectInterface.optionalG()\nMyGObjectInterface.optionalG()',
+        obj.optionalG());
+}
+
+function testGObjectDefersToLastInterfaceOptionalFunction() {
+    const MyOtherMinimalGObject = new Lang.Class({
+        Name: 'MyOtherMinimalGObject',
+        Extends: GObject.Object,
+        Implements: [ MyGObjectInterface, MyOtherGObjectInterface ],
+        Properties: {
+            'interface-prop': GObject.ParamSpec.string('interface-prop',
+                'override', 'override', GObject.ParamFlags.READABLE, 'foobar')
+        },
+
+        _init: function (props={}) {
+            this.parent(props);
+        },
+        requiredG: function () {}
+    });
+    let obj = new MyOtherMinimalGObject();
+    JSUnit.assertEquals('MyOtherGObjectInterface.optionalG()\nMyGObjectInterface.optionalG()',
+        obj.optionalG());
+}
+
+function testGObjectClassMustImplementAllRequiredInterfaces() {
+    JSUnit.assertRaises(() => new Lang.Class({
+        Name: 'MyBadObject',
+        Implements: [ MyOtherGObjectInterface ],
+        required: function () {}
+    }));
+}
+
+function testGObjectClassMustImplementRequiredInterfacesInCorrectOrder() {
+    JSUnit.assertRaises(() => new Lang.Class({
+        Name: 'MyBadObject',
+        Implements: [ MyOtherGObjectInterface, MyGObjectInterface ],
+        required: function () {}
+    }));
+}
+
+function testGObjectInterfaceCanRequireInterfaceFromC() {
+    JSUnit.assertRaises(() => new Lang.Class({
+        Name: 'MyBadObject',
+        Implements: [ MyInitableInterface ]
+    }));
+}
+
+function testGObjectHasInterfaceSignalsAndClassSignals() {
+    let obj = new MyGObject();
+    let interface_signal_emitted = false, class_signal_emitted = false;
+    obj.connect('interface-signal', () => {
+        interface_signal_emitted = true;
+        Mainloop.quit('signal');
+    });
+    obj.connect('class-signal', () => {
+        class_signal_emitted = true;
+        Mainloop.quit('signal');
+    });
+    GLib.idle_add(GLib.PRIORITY_DEFAULT, () => obj.emit('interface-signal'));
+    Mainloop.run('signal');
+    GLib.idle_add(GLib.PRIORITY_DEFAULT, () => obj.emit('class-signal'));
+    Mainloop.run('signal');
+    JSUnit.assertTrue(interface_signal_emitted);
+    JSUnit.assertTrue(class_signal_emitted);
+}
+
+function testGObjectHasInterfacePropertiesAndClassProperties() {
+    let obj = new MyGObject();
+    JSUnit.assertEquals('foobar', obj.interface_prop);
+    JSUnit.assertEquals('meh', obj.class_prop);
+}
+
+// Failing to override an interface property doesn't raise an error but instead
+// logs a critical warning.
+function testGObjectMustOverrideInterfaceProperties() {
+    GLib.test_expect_message('GLib-GObject', GLib.LogLevelFlags.LEVEL_CRITICAL,
+        "Object class * doesn't implement property 'interface-prop' from " +
+        "interface 'ArbitraryGTypeName'");
+    new Lang.Class({
+        Name: 'MyNaughtyObject',
+        Extends: GObject.Object,
+        Implements: [ MyGObjectInterface ],
+        _init: function (props={}) {
+            this.parent(props);
+        },
+        requiredG: function () {}
+    });
+    // g_test_assert_expected_messages() is a macro, not introspectable
+    GLib.test_assert_expected_messages_internal('Gjs', 'testGObjectInterface.js',
+        416, 'testGObjectMustOverrideInterfaceProperties');
+}
+
+// This makes sure that we catch the case where the metaclass (e.g.
+// GtkWidgetClass) doesn't specify a meta-interface. In that case we get the
+// meta-interface from the metaclass's parent.
+function testInterfaceIsOfCorrectTypeForMetaclass() {
+    const MyMeta = new Lang.Class({
+        Name: 'MyMeta',
+        Extends: GObject.Class
+    });
+    const MyMetaObject = new MyMeta({
+        Name: 'MyMetaObject'
+    });
+    const MyMetaInterface = new Lang.Interface({
+        Name: 'MyMetaInterface',
+        Requires: [ MyMetaObject ]
+    });
+    JSUnit.assertTrue(MyMetaInterface instanceof GObject.Interface);
 }
 
 JSUnit.gjstestRun(this, JSUnit.setUp, JSUnit.tearDown);
